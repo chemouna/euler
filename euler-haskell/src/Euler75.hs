a193d2d77d89393e574361802ceb5e1271526e50
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-08-31 01:06:39 +0200

    Start.

---
+++ euler-haskell/src/Euler75.hs
@@ -0,0 +1,12 @@
+module Euler75 where
+
+import Data.Array
+
+-- Singular integer right triangles
+
+-- euler75 :: Int -> Int
+-- euler75 limit = length rightAngles
+
+-- rightAngles :: Int -> [Int]
+-- rightAngles limit = (a, b, c)
+--   where a =
