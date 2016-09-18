915415a85f4c4907f2564879754ac82421ba317b
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-09-18 17:39:24 +0200

    adding a limit for euler 30.

--- euler-haskell/src/Euler30.hs
+++ euler-haskell/src/Euler30.hs
@@ -1,4 +1,3 @@
-
 module Euler30 where
 
 
@@ -6,7 +5,9 @@ digs :: Integer -> [Integer]
 digs 0 = []
 digs x = (x `mod` 10) : digs (x `div` 10)
 
-sumPowersDigits :: Integer
-sumPowersDigits = sum [x | x <- [2..500000], sum (map (^5) (digs x)) == x]
+-- to find the limit of numbers equal to the sum of 5-th powers of their decimal digits.
+-- Since this sum is <= 9^5*d for a d-digit number n >= 10^(d-1),
+-- there cannot be such a number with more than 6 digits. -> upperbound is 999999
 
-
+sumPowersDigits :: [Integer]
+sumPowersDigits = [x | x <- [2..999999], sum (map (^5) (digs x)) == x]
