8084191cb5691cf64b6d8e6154ba1fbfd9de311f
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-17 01:43:56 +0100

    Add an hspec test.

--- euler-haskell/test/Euler12.hs
+++ euler-haskell/test/Euler12.hs
@@ -1,11 +1,18 @@
-
 module Euler12Test where
 
 import Test.QuickCheck
+import Test.Hspec
 
 import Euler12
 
 prop_triangle_numbers (Positive n) =
   triangleNumbers !! (fromInteger n - 1) == sum [1..n]
 
-  
+
+main :: IO ()
+main = hspec $ do
+
+  describe "euler12" $ do
+
+    it "gives 76576500 as a solution" $ do
+      euler12 `shouldBe` 76576500
